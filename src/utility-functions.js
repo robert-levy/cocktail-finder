8c6d1617eacf71adbf80c8b30dee996ff0ec80b7
Robert Levy <36302718+robert-levy@users.noreply.github.com>
2021-09-23 16:21:36 +0100

    favourtites work completed

--- src/utility-functions.js
+++ src/utility-functions.js
@@ -8,34 +8,18 @@ export const getCocktailIngredients = (cocktail) => {
 
 export const getCocktails = (state) => {
     const cocktails = Object.values(state.cocktails.searched[state.selectedLetter])
-    if (cocktails === undefined) return null // should display <NotFound/>
+    if (cocktails === undefined) return null
     return cocktails
 }
 
 // checks if cocktail data for selected letter has already been fetched
 export const containsCocktailData = (state, letter) => state.cocktails.searched[letter]
 
-// checks if given cocktail is in favourites        RETURNING WRONG VALUES FOR RANDOM COCKTAILS AND FAVOURITING ALL OTHERS
-export const checkIsFavourite = (favourites, cocktail) => {
-    favourites.map(favCock => console.log(`favCock : ${favCock.strDrink} ${favCock.idDrink} and cocktail :  ${cocktail.strDrink} ${cocktail.idDrink}`))
-    const isFavourite = favourites.filter(favCock => favCock.idDrink === cocktail.idDrink)
-    // console.log(isFavourite)
-    if(isFavourite.length === 0) return false
-    return true
-}
+// checks if given cocktail is in favourites 
+export const checkIsFavourite = (favourites, cocktail) => favourites.some(favCocktail => favCocktail.idDrink === cocktail.idDrink)
 
 export const preProcessData = (cocktails) => {
-    // letter search returns no cocktails, return []
     if (cocktails === null) return []
-
-    cocktails.forEach(cocktail => {
-        // remove all null values
-        Object.keys(cocktail).forEach(key => cocktail[key] === null && delete cocktail[key])
-    }
-    )
+    cocktails.forEach(cocktail => Object.keys(cocktail).forEach(key => cocktail[key] === null && delete cocktail[key]))
     return cocktails
 }
-
-
-
-
